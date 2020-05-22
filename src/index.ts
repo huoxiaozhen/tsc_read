843371eb234e0e30df3411037060c5cc5693c096
huoxiaozhen <269105294@qq.com>
2020-05-22 17:52:31 +0800

    ''

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,66 @@ enum Roles{
   ADMIN,
   USER
 }
+enum Message{
+  Error = "sorry,error",
+  Success = "Holo,success"
+}
+enum Animal{
+  Dog = 1,
+  Cat = 2
+}
+interface Dog {
+  type: Animal.Dog
+}
+interface Cat {
+  type: Animal.Cat
+}
+let cat1: Cat = {
+  type: Animal.Cat
+}
+const getLength = (taget:string|number):number => {
+  if((taget as string).length){
+    return (taget as string).length
+  }else{
+    return taget.toString().length
+  }
+}
+
+const getFullName = ({firstName, lastName}:{
+  firstName: string;
+  lastName: string;
+}) => {
+  return `${firstName} ${lastName}`
+}
+let full = getFullName({
+  firstName: 'lison',
+  lastName: 'li'
+})
+console.log(full)
+interface info {
+  firstName?: string;
+  lastName: string;
+}
+interface Vegatables{
+  color?:string;
+  type:string;
+}
+const getVegatables = ({color,type}:Vegatables) => {
+  return `${color}${type}`
+}
+getVegatables({
+  type: "tomato",
+  size: 12,
+  price: 1.2
+} as Vegatables)
+
+interface AddFunc{
+  (num1:number,num2:number):number
+}
+let Add:AddFunc = (n1,n2) => n1 + n2
+
+interface RoleDic{
+  [id:number]: string;
+}
+let role:RoleDic = ["super_admin", "admin"]
+console.log(role)
